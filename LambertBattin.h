6d4b978181156edd62a2953e220d4c5d3b38ddea
josorian54 <josorian@unirioja.es>
2023-03-24 20:39:12 +0100

    Initial commit

---
+++ LambertBattin.h
@@ -0,0 +1,11 @@
+//
+// Created by josej on 24/03/2023.
+//
+#include "vector.h"
+#include <cmath>
+#include <iostream>
+using namespace std;
+#ifndef _LAMBERTBATTIN_
+#define _LAMBERTBATTIN_
+void LambertBattin(vector<double> ro, vector<double> r, const char *dm, double Dtsec, vector<double> vo, vector<double>  v);
+#endif
---
+++ seebattk.h
@@ -0,0 +1,11 @@
+//
+// Created by josorian on 22/03/2023.
+//
+#include <cmath>
+#include "vector.h"
+using namespace std;
+
+#ifndef _SEEBATTK_
+#define _SEEBATTK_
+double seebattk(double v);
+#endif //SEEBATTK
